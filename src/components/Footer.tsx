c2a40318a2f1ac77dde3e7125ab7de5b2117b11e
DowonKim <kimdowon1404@gmail.com>
2024-06-11 17:03:32 +0900

    Feat : 개발 진행 중

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,85 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div className="">Footer</div>;
+  return (
+    <div className="px-4 py-24 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
+      {/* TOP */}
+      <div className="flex flex-col md:flex-row justify-between gap-24">
+        {/* LEFT */}
+        <div className="w-full md:W-1/2 lg:w-1/4 flex flex-col gap-8">
+          <Link href="/">
+            <div className="text-2xl tracking-wide">DWSH</div>
+          </Link>
+          <p>Vercel Inc. 123 Townsend St. San Francisco, CA 94107 United States</p>
+          <span className="font-semibold">vercelDowon@gmail.com</span>
+          <span className="font-semibold">+82 10-1354-4879</span>
+          <div className="flex gap-6">
+            <Image src="/facebook.png" alt="facebook" width={16} height={16} />
+            <Image src="/instagram.png" alt="instagram" width={16} height={16} />
+            <Image src="/youtube.png" alt="youtube" width={16} height={16} />
+            <Image src="/pinterest.png" alt="pinterest" width={16} height={16} />
+            <Image src="/x.png" alt="x" width={16} height={16} />
+          </div>
+        </div>
+        {/* CENTER */}
+        <div className="hidden lg:flex justify-between w-1/2">
+          {/* COMPANY */}
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">About Us</Link>
+              <Link href="">Careers</Link>
+              <Link href="">Affiliates</Link>
+              <Link href="">Blog</Link>
+              <Link href="">Contact Us</Link>
+            </div>
+          </div>
+          {/* SHOP */}
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SHOP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">New Arrivals</Link>
+              <Link href="">Accessories</Link>
+              <Link href="">Men</Link>
+              <Link href="">Women</Link>
+              <Link href="">All Products</Link>
+            </div>
+          </div>
+          {/* HELP */}
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="">Customer Service</Link>
+              <Link href="">My Account</Link>
+              <Link href="">Find a Store</Link>
+              <Link href="">Legal & Privacy</Link>
+              <Link href="">Gift Card</Link>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full md:W-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+          <p>Be the first to get the latest news about trends, promotions, and much more.</p>
+          <div className="flex">
+            <input type="text" placeholder="Email address" className="p-4 w-3/4" />
+            <button className="w-1/4 bg-alarm text-white">Join</button>
+          </div>
+          <span className="font-semibold">Payments</span>
+          <div className="flex justify-between">
+            <Image src="/discover.png" alt="discover" width={40} height={20} />
+            <Image src="/skrill.png" alt="skrill" width={40} height={20} />
+            <Image src="/paypal.png" alt="paypal" width={40} height={20} />
+            <Image src="/mastercard.png" alt="mastercard" width={40} height={20} />
+            <Image src="/visa.png" alt="visa" width={40} height={20} />
+          </div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Footer;
