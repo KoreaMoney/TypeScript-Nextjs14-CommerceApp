0c1401ffc0e45f0a0cf79eb6d2f75a08dfaa0802
DowonKim <kimdowon1404@gmail.com>
2024-06-11 17:26:36 +0900

    Feat : 개발 진행 중

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -77,7 +77,19 @@ const Footer = () => {
         </div>
       </div>
       {/* BOTTOM */}
-      <div className=""></div>
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">© Copyright 2024 DWSH. All rights reserved.</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">South Korea | English</span>
+          </div>
+          <div className="">
+            <span className="text-gray-500 mr-4">Currency</span>
+            <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
