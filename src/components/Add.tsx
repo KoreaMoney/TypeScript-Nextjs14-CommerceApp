32cbba0e813df332e5e46d45c09e354425358441
KoreaMoney <kimdowon1404@gmail.com>
2024-06-14 23:45:46 +0900

    개발 진행 중

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,50 @@
+"use client";
+
+import { useState } from "react";
+
 const Add = () => {
-  return <div className="">Add</div>;
+  const [quantity, setQuantity] = useState(1);
+
+  // TEMPORARY
+  const stock = 4;
+
+  const handleQuantity = (type: "dec" | "inc") => {
+    if (type === "dec" && quantity > 1) {
+      setQuantity((prev) => prev - 1);
+    }
+    if (type === "inc" && quantity < stock) {
+      setQuantity((prev) => prev + 1);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h4 className="font-medium">Choose a Quantity</h4>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-4">
+          <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
+            <button className="cursor-pointer text-xl" onClick={() => handleQuantity("dec")}>
+              -
+            </button>
+            {quantity}
+            <button className="cursor-pointer text-xl" onClick={() => handleQuantity("inc")}>
+              +
+            </button>
+          </div>
+          <div className="text-xs">
+            Only <span className="text-red-500">4 items</span> left!
+            <br /> {"Don't miss it"}
+          </div>
+        </div>
+        <button
+          className="w-36 text-sm rounded-3xl ring-1 ring-alarm text-alarm py-2 px-4 hover:bg-alarm hover:text-white 
+        disabled:cursor-not-allowed disabled:bg-blue-100 disabled:text-white disabled:ring-none"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Add;
